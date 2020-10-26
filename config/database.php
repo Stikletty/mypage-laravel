9e1c9e3c49620b49dadc7495cad9e10a43d7a2e8
Stikletty <troli8@gmail.com>
2020-10-27 00:29:34 +0100

    CI delete

--- config/database.php
+++ config/database.php
@@ -42,11 +42,6 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-        'sqlite_testing' => [
-            'driver'   => 'sqlite',
-            'database' => storage_path('storage/database.sqlite'),
-            'prefix'   => '',
-        ],
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
