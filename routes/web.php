5591f8a41071c96dbbe642b53841ff5a2ddce21c
Stikletty <troli8@gmail.com>
2020-10-27 01:55:17 +0100

    migration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/run-migrations', function () {
+    Artisan::call('migrate', ["--force" => true ]);
+    Artisan::call('db:seed');
+    return;
+});
 
 Route::get('/', function () {
     return view('welcome');
